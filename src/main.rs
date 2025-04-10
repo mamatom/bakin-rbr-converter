f535738d7bd230578dc3abdcb04f35dad3e2fbf6
mamatom <icezamamatom@gmail.com>
2025-04-11 04:35:00 +0700

    batch files parsing and encoding

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::{f32::consts::E, fmt::Error, u128, u64, usize};
+use std::{f32::consts::E, fmt::{Error}, fs, path::{Path, PathBuf}, u128, u64, usize};
+use anyhow::Context;
 use binrw::{prelude::*, Endian::Big, NullString};
+use clap::{command, Parser, Subcommand};
 use leb128;
 use hex_literal::hex;
 use serde::{Deserialize, Serialize};
 use std::io::Cursor;
 
-use binrw::{prelude::*, Endian, io::{Write,Seek, Read, SeekFrom}};
+use binrw::{Endian, io::{Write,Seek, Read, SeekFrom},helpers::until_eof};
 
 #[derive(Debug, Clone,Copy)]
 struct LEB128(u64);
@@ -126,7 +128,8 @@ pub struct Event {
 #[derive(BinRead, BinWrite, Debug, Clone,Serialize, Deserialize)]
 pub struct EventData {
     #[br(parse_with = read_until_null)]
-    #[serde(serialize_with = "serialize_bytes_as_hex")]
+    #[serde(serialize_with = "serialize_bytes_as_hex",
+            deserialize_with = "deserialize_bytes_from_hex")]
     pub data: Vec<u8>,
     seperator: u8,
     #[br(parse_with = parse_variables, args(data.clone()))]
@@ -143,7 +146,7 @@ pub enum EventDataType {
     
     #[br(pre_assert(code == 0x02))]
     
-    U128(#[serde(serialize_with = "serialize_u128_as_hex")] u128),
+    U128(#[serde(serialize_with = "serialize_u128_as_hex", deserialize_with = "deserialize_u128_from_hex")] u128),
     
     #[br(pre_assert(code == 0x03))]
     Text(SizedString),
@@ -156,7 +159,8 @@ pub enum EventDataType {
 
     #[br(pre_assert(code == 0x06))]
     Position {
-        #[serde(serialize_with = "serialize_u128_as_hex")]
+        #[serde(serialize_with = "serialize_u128_as_hex",
+            deserialize_with = "deserialize_u128_from_hex")]
         value: u128,
         data: u32,//TODO: tobe determined if it's a u32 + u8 or u32 + SizedString
         data2: u8,
@@ -173,7 +177,8 @@ pub enum EventDataType {
         #[brw(if(0x01 == array_type.clone()))]
         value1: u32,
         #[brw(if(0x02 == array_type.clone()))]
-        #[serde(serialize_with = "serialize_u128_as_hex")]
+        #[serde(serialize_with = "serialize_u128_as_hex",
+            deserialize_with = "deserialize_u128_from_hex")]
         value2:u128,
         #[brw(if(0x03 == array_type.clone()))]
         value3: SizedString,
@@ -226,10 +231,14 @@ fn parse_variables<R: Read + Seek>(
 struct Section{
     //section_length: u32,
     section_type: u16,
-    #[serde(serialize_with = "serialize_u128_as_hex")]
+
+    #[serde(serialize_with = "serialize_u128_as_hex",
+            deserialize_with = "deserialize_u128_from_hex")]
     data: u128,
     section_data: SectionData,
-    #[serde(serialize_with = "serialize_bytes_as_hex")]
+
+    #[serde(serialize_with = "serialize_bytes_as_hex",
+            deserialize_with = "deserialize_bytes_from_hex")]
     unparsed_bytes: Vec<u8>,
 }
 
@@ -330,7 +339,7 @@ enum SectionData {
         eventsheet_section_end: u16,
     },
 
-    #[br(pre_assert(code == 0x0001))]
+    #[br(pre_assert(code == 0x0007))]
     EntityHeader{
         object_name: SizedString,
         data: u16,
@@ -343,7 +352,8 @@ enum SectionData {
     ItemData{
         name: SizedString,
         note: SizedString,
-        #[serde(serialize_with = "serialize_u128_as_hex")]
+        #[serde(serialize_with = "serialize_u128_as_hex",
+            deserialize_with = "deserialize_u128_from_hex")]
         data: u128,
         data2: u8,
         description: SizedString,
@@ -362,46 +372,176 @@ where
     serializer.serialize_str(&hex_string)
 }
 
+fn deserialize_bytes_from_hex<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let hex_string = String::deserialize(deserializer)?;
+    hex::decode(&hex_string).map_err(serde::de::Error::custom)
+}
+
+
+
 fn serialize_u128_as_hex<S>(value: &u128, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: serde::Serializer,
 {
-    let hex_string = format!("0x{:032x}", value);
+    let hex_string = format!("{:032x}", value);
     serializer.serialize_str(&hex_string)
 }
 
-
-fn main() {
-    let item_section_hex = hex!("E2 05 00 00 00 0C 1B 73 43 53 DD 4B 7D 47 B9 10 67 D3 0B 96 54 73 05 49 74 65 6D 73 05 4E 6F 74 65 73 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 04 44 65 73 63 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 63 00 00 00 00 00 00 00 E9 C6 17 E9 9B 81 67 41 A5 FC C6 F8 5B FB C7 33 CE 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 06 00 00 00 8C 11 BC 82 E0 38 09 43 AA B1 09 A6 70 D5 31 65 00 00 00 00 A7 1E 41 99 3E 2D 70 41 BC EA 3D 83 77 FE 3E F1 00 00 00 00 4F 38 FC 1F 29 55 F8 4D B8 8C 14 34 C6 3F 02 F5 00 00 00 00 2A 50 7F B4 CC 22 B8 46 91 1B C9 5A 0D FE AC A1 00 00 00 00 F2 14 2F 6F E8 26 39 4D 82 70 7D 9A 3B 1A 20 62 00 00 00 00 ED 1D 04 B1 86 BC 6B 45 AA 6D 9E 65 BA E8 35 9C 00 00 00 00 12 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 6D 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 06 00 00 00 01 9C A1 9E AC 82 70 42 90 26 4A 68 72 AA A2 83 00 00 00 00 0D A1 B1 C9 5B 8A 87 4A A4 4B 9A 3E E5 57 04 76 00 00 00 00 36 D9 BA 9B EC 98 7C 4B 92 2A 9A CC 31 FE 62 48 00 00 00 00 32 A9 3B 8E B8 2A 09 48 8B 23 03 B2 D3 84 22 A4 00 00 00 00 98 27 1B CB F8 4A 35 42 8B 33 69 34 88 5F 17 A5 00 00 00 00 19 3A 85 B4 42 01 30 4A B3 1A 27 4C F4 C5 E9 91 00 00 00 00 06 00 00 00 8C 11 BC 82 E0 38 09 43 AA B1 09 A6 70 D5 31 65 00 00 00 00 A7 1E 41 99 3E 2D 70 41 BC EA 3D 83 77 FE 3E F1 00 00 00 00 4F 38 FC 1F 29 55 F8 4D B8 8C 14 34 C6 3F 02 F5 00 00 00 00 2A 50 7F B4 CC 22 B8 46 91 1B C9 5A 0D FE AC A1 00 00 00 00 F2 14 2F 6F E8 26 39 4D 82 70 7D 9A 3B 1A 20 62 00 00 00 00 ED 1D 04 B1 86 BC 6B 45 AA 6D 9E 65 BA E8 35 9C 00 00 00 00 00 00 00 00 84 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00 00 00 00 00 00 42 28 6D 61 78 28 61 2E 61 74 6B 20 2F 20 32 2E 35 20 2D 20 62 2E 64 65 66 20 2F 20 34 2C 20 30 29 20 2B 20 61 2E 65 61 74 6B 20 2A 20 62 2E 65 64 65 66 29 20 2A 20 72 61 6E 64 28 30 2E 39 2C 20 31 29 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 9C A1 9E AC 82 70 42 90 26 4A 68 72 AA A2 83 01 00 00 00 00 00 00 00 01 00 00 00 00 0A 00 00 00 00 00 00 00 00 00 80 3F 00 04 00 00 00 00 00 00 00 01 1B 73 43 53 DD 4B 7D 47 B9 10 67 D3 0B 96 54 73 5E 02 00 00 09 00 00 00 00 00 00 00 19 00 00 00 2E DD EE 8D 03 3E 4B 40 81 4C 42 41 F2 19 A8 0C 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 B2 7A D1 97 D2 AA 86 4B 9C E4 7C 46 C4 E2 5B AF 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 2D 12 63 0F 40 CF DA 47 91 20 F0 9D 46 CF C6 54 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 73 9E 76 69 58 98 60 48 BB E8 45 9E 3F 6F E2 81 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 04 54 C0 83 E9 E9 10 4A 85 A3 D6 C4 40 DB CA FD 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 88 BB EE 1D B0 CD D1 47 85 A8 6B 3B 93 B8 B9 31 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 D8 FE A0 09 BB F7 93 40 85 7E 2E 15 B5 E1 EE 7A 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 5F 20 BD 22 55 C1 3C 4C 84 15 D6 C3 EB 03 C1 7F 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 5F 67 94 A4 F8 ED 59 45 9F DD 37 AA A8 39 23 1D 00 00 00 00 00 00 00 00 01 09 00 00 00 00 00 00 00 19 00 00 00 2E DD EE 8D 03 3E 4B 40 81 4C 42 41 F2 19 A8 0C 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 B2 7A D1 97 D2 AA 86 4B 9C E4 7C 46 C4 E2 5B AF 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 2D 12 63 0F 40 CF DA 47 91 20 F0 9D 46 CF C6 54 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 73 9E 76 69 58 98 60 48 BB E8 45 9E 3F 6F E2 81 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 04 54 C0 83 E9 E9 10 4A 85 A3 D6 C4 40 DB CA FD 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 88 BB EE 1D B0 CD D1 47 85 A8 6B 3B 93 B8 B9 31 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 D8 FE A0 09 BB F7 93 40 85 7E 2E 15 B5 E1 EE 7A 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 5F 20 BD 22 55 C1 3C 4C 84 15 D6 C3 EB 03 C1 7F 00 00 00 00 00 00 00 00 01 00 00 00 00 19 00 00 00 5F 67 94 A4 F8 ED 59 45 9F DD 37 AA A8 39 23 1D 00 00 00 00 00 00 00 00 01 00 00 00 00");
-    let event_section_hex = hex!("EB 01 00 00 10 07 36 44 18 E9 8E 83 AC 42 B3 19 91 8B 92 22 BC C6 00 00 00 02 00 00 00 10 00 00 00 1E 00 00 00 00 00 00 00 01 03 03 03 01 04 04 04 00 03 00 00 00 07 43 68 6F 69 63 65 31 07 43 68 6F 69 63 65 32 07 43 68 6F 69 63 65 33 04 00 00 00 00 00 00 1D 00 00 00 01 00 00 00 03 01 01 00 0D 53 65 6C 65 63 74 43 68 6F 69 63 65 31 02 00 00 00 00 00 00 00 1D 00 00 00 01 00 00 00 03 01 01 00 0F 53 65 6C 65 63 74 43 68 6F 69 63 65 31 5F 31 02 00 00 00 00 00 00 00 1E 00 00 00 01 00 00 00 01 03 03 01 04 04 00 02 00 00 00 07 43 68 6F 69 63 65 41 07 43 68 6F 69 63 65 42 04 00 00 00 00 00 1D 00 00 00 02 00 00 00 03 01 01 00 0D 53 65 6C 65 63 74 43 68 6F 69 63 65 41 02 00 00 00 00 00 00 00 4A 00 00 00 01 00 00 00 01 00 01 00 00 00 1D 00 00 00 02 00 00 00 03 01 01 00 0D 53 65 6C 65 63 74 43 68 6F 69 63 65 42 02 00 00 00 00 00 00 00 1D 00 00 00 02 00 00 00 03 01 01 00 0F 53 65 6C 65 63 74 43 68 6F 69 63 65 42 5F 32 02 00 00 00 00 00 00 00 48 00 00 00 01 00 00 00 00 1D 00 00 00 01 00 00 00 03 01 01 00 0F 53 65 6C 65 63 74 43 68 6F 69 63 65 31 5F 32 02 00 00 00 00 00 00 00 4A 00 00 00 00 00 00 00 01 00 01 00 00 00 1D 00 00 00 01 00 00 00 03 01 01 00 0D 53 65 6C 65 63 74 43 68 6F 69 63 65 32 02 00 00 00 00 00 00 00 4A 00 00 00 00 00 00 00 01 00 02 00 00 00 1D 00 00 00 01 00 00 00 03 01 01 00 0D 53 65 6C 65 63 74 43 68 6F 69 63 65 33 02 00 00 00 00 00 00 00 48 00 00 00 00 00 00 00 00 1D 00 00 00 00 00 00 00 03 01 01 00 0A 45 6E 64 4D 65 73 73 61 67 65 02 00 00 00 00 00 00 00 00 00");
-    let unknow_section_hex = hex!("21 00 00 00 01 01 36 44 18 E9 8E 83 AC 42 B3 19 91 8B 92 22 BC C6 05 68 65 6C 6C 6F 03 03 03 03 03 03 03 03 00 00 00");
-    let mut cursor = Cursor::new(event_section_hex);
-    let mut test:Section = cursor.read_le().unwrap();
-    println!("String: {:#?}", test);
-    let json = serde_json::to_string_pretty(&test).unwrap();
-    std::fs::write("output.json", json).unwrap();
-
-   /*match test.section_data {
-       SectionData::ItemData {ref mut name,ref mut note, data, data2,ref mut description, data3 } => {
-        name.set_value("ItemName111111".to_string());
-        note.set_value("Note111111".to_string());
-        description.set_value("Description111111".to_string());
-       },
-       _ => {}
-   }*/
-
-   
+fn deserialize_u128_from_hex<'de, D>(deserializer: D) -> Result<u128, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let hex_string = String::deserialize(deserializer)?;
+    u128::from_str_radix(&hex_string, 16).map_err(serde::de::Error::custom)
+}
 
 
 
-    let mut text2 = Cursor::new(vec![]);
-    text2.write_le(&test).unwrap();
-    println!("Serialized bytes:");
-    for byte in text2.get_ref() {
-        print!("{:02x} ", byte);
+#[binrw]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct RbrFile {
+    #[br(parse_with = validate_header)]
+    header: RbrHeader,
+    #[br(parse_with = until_eof)]
+    sections: Vec<Section>,
+}
+
+
+#[binrw]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[brw(magic = b"YUKAR")]
+struct RbrHeader {
+    #[brw(big)]
+    version: u16,
+    unknown: [u8;10],
+}
+
+fn validate_header<R: Read + Seek>(reader: &mut R, endian: Endian, _: ()) -> BinResult<RbrHeader> {
+    let header = RbrHeader::read_options(reader,endian,());
+    match header {
+        Ok(_header) => Ok(_header),
+        Err(_) => return Err(binrw::Error::Custom {
+            pos: reader.stream_position().unwrap_or(0),
+            err: Box::new(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Invalid RBR file header",
+            )),
+        })
     }
+    
+}
 
-    /*let mut test2_raw = Cursor::new(text2.into_inner());
-    let test2:Section = test2_raw.read_le().unwrap();
-    println!("String: {:#?}", test2);*/
+
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+    
+    #[arg(short, long)]
+    /// Input directory
+    input: PathBuf,
+    
+    #[arg(short, long)]
+    /// Output directory
+    output: PathBuf,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Convert binary RBR files to JSON
+    Parse,
+    /// Convert JSON files back to binary RBR format
+    Encode,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    
+    match &cli.command {
+        Commands::Parse => process_files(
+            &cli.input,
+            &cli.output,
+            "rbr",
+            "json",
+            parse_rbr_file,
+        )?,
+        Commands::Encode => process_files(
+            &cli.input,
+            &cli.output,
+            "json",
+            "rbr",
+            encode_json_to_rbr,
+        )?,
+    }
+    
+    Ok(())
+}
+
+
+fn process_files(
+    input_dir: &Path,
+    output_dir: &Path,
+    input_ext: &str,
+    output_ext: &str,
+    processor: impl Fn(&Path, &Path) -> anyhow::Result<()>,
+) -> anyhow::Result<()> {
+    for entry in walkdir::WalkDir::new(input_dir) {
+        let entry = entry?;
+        let path = entry.path();
+        
+        if path.is_file() && path.extension().map_or(false, |e| e == input_ext) {
+            let relative_path = path.strip_prefix(input_dir)?;
+            let output_path = output_dir.join(relative_path).with_extension(output_ext);
+            
+            if let Some(parent) = output_path.parent() {
+                fs::create_dir_all(parent)?;
+            }
+            
+            processor(path, &output_path).with_context(|| format!(
+                "Failed processing {}",
+                path.display()
+            ))?;
+        }
+    }
+    Ok(())
+}
+
+
+fn parse_rbr_file(input_path: &Path, output_path: &Path) -> anyhow::Result<()> {
+    // Read binary file
+    let data = fs::read(input_path)?;
+    let mut cursor = Cursor::new(data);
+    
+    // Parse using your existing BinRead implementation
+    let file = RbrFile::read_options(&mut cursor,Endian::Little,())?;
+    
+    // Serialize to JSON
+    let json = serde_json::to_string_pretty(&file)?;
+    
+    // Write JSON file
+    fs::write(output_path, json)?;
+    
+    Ok(())
+}
+
+/// Encode JSON file back to binary RBR format
+fn encode_json_to_rbr(input_path: &Path, output_path: &Path) -> anyhow::Result<()> {
+    // Read JSON file
+    let json = fs::read_to_string(input_path)?;
+    
+    // Deserialize from JSON
+    let file: RbrFile = serde_json::from_str(&json)?;
+    
+    // Write binary file
+    let mut cursor = Cursor::new(Vec::new());
+    file.write_options(&mut cursor,Endian::Little,())?;
+    fs::write(output_path, cursor.into_inner())?;
+    
+    Ok(())
 }
